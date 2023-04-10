fe050fb9292892328a06b3ec1ed32d0ff560d77d
Hyun-Tae Jin <htjmario@gmail.com>
2023-04-10 10:36:16 -0400

    added newlines, logic/regex/attribute/rent fixes

--- property_investment_run.py
+++ property_investment_run.py
@@ -1,5 +1,5 @@
-import json
 from rental_property_calculator import User, Income, Expenses, CashFlow, CashReturn
+from IPython.display import clear_output as clear
 
 class PropertyInvestmentCalculator:
     def __init__(self):
@@ -19,7 +19,7 @@ class PropertyInvestmentCalculator:
     
     def register(self):
         while True:
-            print("[<-- New User Registration -->]")
+            print("\n[<-- New User Registration -->]")
             username = input("Enter a new username: ")
             if username in self.users:
                 print("Username already exists. Please choose another one.")
@@ -27,17 +27,17 @@ class PropertyInvestmentCalculator:
                 break
         password = input("Enter a new password: ")
         self.users[username] = password
-        print("Registration successful.")
+        print("\nRegistration successful.")
         return username
 
     def login(self):
         username = input("Enter your username: ")
         password = input("Enter your password: ")
         if username in self.users and self.users[username] == password:
-            print("Logged in as " + username)
+            print("\nLogged in as " + username)
             return True
         else:
-            print("Invalid username or password.")
+            print("\nInvalid username or password.")
             return False
 
     def logout(self):
@@ -45,17 +45,20 @@ class PropertyInvestmentCalculator:
         print("Logged out successfully.")
         
     def get_user_inputs(self):
-        state, year = None, None
         while True:
-            state = input("1. State: ").capitalize()
+            state = input("1. State: ").lower()
             if state not in self.states:
                 print("Invalid input: State not found. Please enter a valid state.")
                 continue
+            else:
+                break
+        while True:
             year = self.validate_inputs("2. Year (between 1940 and 2000): ", int)
             if year < 1940 or year > 2000:
                 print("Invalid input: year must be an integer and between 1940 and 2000.")
                 continue
-            break
+            else:
+                break
 
         self.user = User(state, year)
         self.user.access_rent_data()
@@ -73,14 +76,27 @@ class PropertyInvestmentCalculator:
         laundry = self.validate_inputs("12. Laundry income (default: 0): ", default=0)
         storage = self.validate_inputs("13. Storage income (default: 0): ", default=0)
         misc = self.validate_inputs("14. Miscellaneous income (default: 0): ", default=0)
-                
-        self.user = User(state, year, insurance, yard, vacancy, repairs, capex, mortgage, down, closing, rehab, laundry, storage, misc)
+        
+        self.user.insurance = insurance
+        self.user.yard = yard
+        self.user.vacancy = vacancy
+        self.user.repairs = repairs
+        self.user.capex = capex
+        self.user.mortgage = mortgage
+        self.user.down = down
+        self.user.closing = closing
+        self.user.rehab = rehab
+        self.user.laundry = laundry
+        self.user.storage = storage
+        self.user.misc = misc
     
     def calculate(self):
-        income = Income(self.user)
-        expenses = Expenses(self.user, self.user.insurance, self.user.yard, self.user.vacancy, self.user.repairs, self.user.capex, self.user.mortgage)
+        clear()
+        user = User(self.user.state, self.user.year, self.user.insurance, self.user.yard, self.user.vacancy, self.user.repairs, self.user.capex, self.user.mortgage, self.user.down, self.user.closing, self.user.rehab, self.user.laundry, self.user.storage, self.user.misc)
+        income = Income(user)
+        expenses = Expenses(user)
         cash_flow = CashFlow(income, expenses)
-        cash_return = CashReturn(cash_flow, self.user.down, self.user.closing, self.user.rehab)
+        cash_return = CashReturn(cash_flow, user)
         print(cash_return)
        
     def run(self):
@@ -89,7 +105,7 @@ class PropertyInvestmentCalculator:
             if action == "register":
                 username = self.register()
                 if username:
-                    print("Please log in with your newly registered account.")
+                    print("Please log in with your newly registered account\n")
             elif action == "login":
                 is_logged_in = self.login()
                 if is_logged_in:
@@ -100,7 +116,7 @@ class PropertyInvestmentCalculator:
                     print("No users found. Please register.")
                     username = self.register()
                     if username:
-                        print("Please log in with your newly registered account.")
+                        print("Please log in with your newly registered account.\n")
             elif action == "exit":
                 print("Good bye.")
                 break
--- rental_property_calculator.py
+++ rental_property_calculator.py
@@ -17,8 +17,7 @@ class User():
         self.laundry = laundry
         self.storage = storage
         self.misc = misc
-        self.rent = 0
-        self.data = self.access_rent_data()
+        self.rent = self.access_rent_data()
         if self.state:
             self.tax_rate, self.hoa_rate, self.propman_rate, self.utilities = self.get_rates()
         else:
@@ -27,43 +26,44 @@ class User():
     def access_rent_data(self):
         with open('grossrents.txt') as f:
             rents = f.readlines()
-        pattern = re.compile(r'^([A-Z][a-z]+)\s+(\d+|NA)\s+(\d+|NA)\s+(\d+|NA)\s+(\d+|NA)\s+(\d+|NA)\s+(\d+|NA)\s+(\d+|NA)')
+        pattern = re.compile(r'^([A-Za-z\s\.]+)\s+\$?(\d+|\bNA\b)\s+\$?(\d+|\bNA\b)\s+\$?(\d+|\bNA\b)\s+\$?(\d+|\bNA\b)\s+\$?(\d+|\bNA\b)\s+\$?(\d+|\bNA\b)\s+\$?(\d+|\bNA\b)$')
         for line in rents:
             match = pattern.search(line)
             if match:
                 state, rent2000, rent1990, rent1980, rent1970, rent1960, rent1950, rent1940 = match.groups()
+                state = ' '.join(state.split()).lower()
                 if state == self.state:
                     rent_values = [rent2000, rent1990, rent1980, rent1970, rent1960, rent1950, rent1940]
                     rents_by_decade = [int(rent) if rent != 'NA' else None for rent in rent_values]
                     start_year = 2000
                     end_year = 1940
-                    increment = (rents_by_decade[0] - rents_by_decade[-1]) / (start_year - end_year)
+                    valid_rents = [(index * 10, rent) for index, rent in enumerate(rents_by_decade) if rent is not None]
+                    increment = (valid_rents[0][1] - valid_rents[-1][1]) / (valid_rents[0][0] - valid_rents[-1][0])
                     for year in range(start_year, end_year - 1, -1):
                         if self.year == year:
-                            self.rent = rents_by_decade[(start_year - year) // 10] - increment * (start_year - year) % 10
-                            break
+                            rent = rents_by_decade[(start_year - year) // 10] - increment * (start_year - year) % 10
+                            return rent
                         elif year > self.year > year - 10:
-                            self.rent = rents_by_decade[(start_year - year) // 10] - increment * (start_year - self.year) % 10
-                            break
+                            rent = rents_by_decade[(start_year - year) // 10] - increment * (start_year - self.year) % 10
+                            return rent
                         
     def get_rates(self):
         with open('rates.json', 'r') as f:
             rates = json.load(f)
-        return rates[self.state]
+        formatted_rates = {key.lower(): value for key, value in rates.items()}
+        return formatted_rates[self.state]
     
     def get_states(self):
         with open('rates.json', 'r') as f:
             rates = json.load(f)
-        return list(rates.keys())
+        return [state.lower() for state in rates.keys()]
                 
 class Income():
-    def __init__(self, user, laundry=0, storage=0, misc=0):
-        self.state = user.state
-        self.year = user.year
+    def __init__(self, user):
         self.rental = user.rent
-        self.laundry = laundry
-        self.storage = storage
-        self.misc = misc
+        self.laundry = user.laundry
+        self.storage = user.storage
+        self.misc = user.misc
         self.total = self.calculate_income()
 
     def calculate_income(self):
@@ -72,19 +72,19 @@ class Income():
         return self.total
 
     def __repr__(self):
-        return f'Your total monthly income is ${self.total:,.2f}'
+        return f'\nYour total monthly income is ${self.total:,.2f}\n'
        
 class Expenses():
-    def __init__(self, user, insurance, yard, vacancy, repairs, capex, mortgage):
+    def __init__(self, user):
         self.tax = user.rent * user.tax_rate
         self.hoa = user.rent * user.hoa_rate
         self.propman = user.rent * user.propman_rate
-        self.insurance = insurance
-        self.yard = yard
-        self.vacancy = vacancy
-        self.repairs = repairs
-        self.capex = capex
-        self.mortgage = mortgage
+        self.insurance = user.insurance
+        self.yard = user.yard
+        self.vacancy = user.vacancy
+        self.repairs = user.repairs
+        self.capex = user.capex
+        self.mortgage = user.mortgage
         self.utilities = user.utilities
         self.total = self.calculate_expense()
         
@@ -94,7 +94,7 @@ class Expenses():
         return self.total
         
     def __repr__(self):
-        return f'Your total monthly expenses are ${self.total:,.2f}'
+        return f'Your total monthly expenses are ${self.total:,.2f}\n'
 
         
 class CashFlow:
@@ -109,14 +109,14 @@ class CashFlow:
         return self.total
 
     def __repr__(self):
-        return f'Your total annual cash flow is ${self.total:,.2f}'
+        return f'Your total annual cash flow is ${self.total:,.2f}\n'
 
 class CashReturn():
-    def __init__(self, cash_flow, down, closing, rehab, misc=0):
-        self.down = down
-        self.closing = closing
-        self.rehab = rehab
-        self.misc = misc
+    def __init__(self, cash_flow, user):
+        self.down = user.down
+        self.closing = user.closing
+        self.rehab = user.rehab
+        self.misc = user.misc
         self.total_investment = self.calculate_investment()
         self.cash_flow = cash_flow.total
         self.return_rate = self.calculate_return()
@@ -128,4 +128,4 @@ class CashReturn():
         return (self.cash_flow / self.total_investment) * 100
 
     def __repr__(self):
-        return f'Your annual cash-on-cash return is {self.return_rate:.2f}%'
+        return f'Your annual cash-on-cash return is {self.return_rate:.2f}%\n'
